59cdd27995c9097fba83fe053b8ec3beebb086b4
Maria Negri <mariamnegri@gmail.com>
2021-09-15 20:05:58 -0300

    First version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-
+import React,{useReducer} from 'react';
+import reducer, { initialState } from "./reducers/index.js";
+import {applyNumber,changeOperation,clearDisplay, m_c, m_plus,m_r} from './actions/index.js';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+
+
+  const [state, dispatch] = useReducer(reducer,initialState)
+
+  const handleClear =() =>{
+    dispatch(clearDisplay());
+    console.log('displayyy',dispatch(clearDisplay()))
+  }
+
+  const handleMPlus =() =>{dispatch(m_plus())}
+  const handleMr =() =>{dispatch(m_r())}
+  const handleMc =() =>{dispatch(m_c())}
+
+  const handleSum =() =>{dispatch(changeOperation("+"))}
+  const handleSubst =() =>{ dispatch(changeOperation("-"))}
+  const handleMult =() => {dispatch(changeOperation("*"))}
+
+  const handleClick =()  =>{dispatch(applyNumber(1))}
+  const handleClick2 =() =>{dispatch(applyNumber(2))}
+  const handleClick3 =() =>{dispatch(applyNumber(3))}
+  const handleClick4 =() =>{dispatch(applyNumber(4))}
+  const handleClick5 =() =>{dispatch(applyNumber(5))}
+  const handleClick6 =() =>{dispatch(applyNumber(6))}
+  const handleClick7 =() =>{dispatch(applyNumber(7))}
+  const handleClick8 =() =>{dispatch(applyNumber(8))}
+  const handleClick9 =() =>{dispatch(applyNumber(9))}
+
 
   return (
     <div className="App">
@@ -17,44 +45,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b>{state.operation}</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={handleMPlus} value={"M+"}/>
+              <CalcButton  onClick={handleMr} value={"MR"}/>
+              <CalcButton  onClick={handleMc} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={handleClick} value={1}/>
+              <CalcButton onClick={handleClick2} value={2}/>
+              <CalcButton onClick={handleClick3} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={handleClick4} value={4}/>
+              <CalcButton onClick={handleClick5} value={5}/>
+              <CalcButton onClick={handleClick6} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={handleClick7} value={7}/>
+              <CalcButton onClick={handleClick8} value={8}/>
+              <CalcButton onClick={handleClick9} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={handleSum} value={"+"}/>
+              <CalcButton onClick={handleMult} value={"*"}/>
+              <CalcButton onClick={handleSubst} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onclick ={handleClear} value={"CE"}/>
             </div>
 
           </form>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
-export const ADD_ONE = "ADD_ONE";
 
+export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const M_PLUS = "M_PLUS";
+export const M_R= "M_R";
+export const M_C= "M_C";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,22 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (text) => {
+    return({type:CHANGE_OPERATION, payload:text});
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY});
+}
+
+export const m_plus = () => {
+    return({type:M_PLUS});
+}
+export const m_r = () => {
+    return({type:M_R});
+}
+export const m_c = () => {
+    return({type:M_C});
 }
