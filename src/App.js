671e7051d07d217d113f28d6584baf966c8d243f
Maria Negri <mariamnegri@gmail.com>
2021-09-15 22:27:21 -0300

    Final Push

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton onclick ={handleClear} value={"CE"}/>
+              <CalcButton onClick ={handleClear} value={"CE"}/>
             </div>
 
           </form>
